1c490a5eafa2971eccb991a713bec349c2708877
Jeremiah Flores <99288063+GerimiyaF@users.noreply.github.com>
2022-02-11 22:41:38 +0000

    fixed issue with numbers

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import itertools
+class NumberError(Exception):
+  pass
 
 # global vars
 total = 0
 player = itertools.cycle('12')
 
 while True:
-	print(f"\nThe current total is {total}.")
-	step = int(input(f"Player {next(player)}, choose a number between 1 and 10 to add: "))
-	total += step
-	if total == 100:
-		print(f"Player {player} wins!")
-		break
-	
-	
-	
+  print(f"\nThe current total is {total}.")
+  while True:
+    step = (input(f"Player {next(player)}, choose a number between 1 and 10 to add: "))
+    try:
+      if int(step)%1 != 0:
+        raise NumberError
+      elif int(step)>10:
+        raise NumberError
+      elif int(step)<1:
+        raise NumberError
+    except NumberError:
+      print("ONE IN TEN\n")
+    except ValueError:
+      print("Whole Numbers Only\n")
+    else:
+      break
+  total += int(step)
+  if total == 100:
+	  print(f"Player {player} wins!")
+	  break
