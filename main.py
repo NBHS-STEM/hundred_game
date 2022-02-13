3b59d11921b90a13272b33a9a715091f843d07c6
BenjaminDiaz7 <>
2022-02-13 22:22:29 +0000

    Fixed an issue regarding validation of guesses

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ import itertools
 # global vars
 total = 0
 player = itertools.cycle('12')
+class NumError(Exception):
+  pass
 
+print(f"\nThe current total is {total}.")
 while True:
-	print(f"\nThe current total is {total}.")
-	step = int(input(f"Player {next(player)}, choose a number between 1 and 10 to add: "))
-	total += step
-	if total == 100:
-		print(f"Player {player} wins!")
-		break
-	
-	
-	
+  step = input(f"Player {next(player)}, choose a number to add: ")
+  try:
+    if int(step)<1:
+      raise NumError 
+    elif int(step)>10:
+      raise NumError
+    elif int(step)%1 != 0:
+      raise NumError
+    else:
+      pass
+  except NumError:
+    print("CORRECT NUMBERS PLEASE\n")
+  except ValueError:
+    print("AN INTEGER, PLEASE\n")
+  else:
+    break
+total += int(step)
+if total == 100:
+    print(f"Player {player} wins!")
