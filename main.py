843fe67fd6660bb42793f60af13d5fb2cb1b08d6
Shehroz Khan <88639037+Shehroz24@users.noreply.github.com>
2022-02-14 16:22:59 +0000

    raised number errors

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import itertools
-
+class NumberError(Exception):
+  pass
 # global vars
 total = 0
 player = itertools.cycle('12')
 
+step = (input(f"Player {next(player)},a number between 1 and 10: "))
+
 while True:
-	print(f"\nThe current total is {total}.")
-	step = int(input(f"Player {next(player)}, choose a number between 1 and 10 to add: "))
-	total += step
-	if total == 100:
-		print(f"Player {player} wins!")
-		break
+  print(f"\nThe current total is {total}.")
+  try:
+    if int(step)%1 != 0:
+      raise NumberError
+    elif int(step)<1:
+      raise NumberError
+    elif int(step)>10:
+      raise NumberError
+    else:
+      pass
+  except ValueError:
+    print("ints only")
+  except NumberError:
+    print("Not Valid")
+  else:
+    break
+
+  total += step
+  if total == 100:
+	  print(f"Player {player} wins!")
+	  break
 	
 	
 	
