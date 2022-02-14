725e5c971a9b9338abf84c87665c746b280c493a
denricoNBHS <>
2022-02-14 14:49:58 +0000

    Add input validation to main.py

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import itertools
 total = 0
 player = itertools.cycle('12')
 
+# Game loop
 while True:
-	print(f"\nThe current total is {total}.")
-	step = int(input(f"Player {next(player)}, choose a number between 1 and 10 to add: "))
+	# Validation loop
+	while True:
+		print(f"\nThe current total is {total}.")
+		try:
+			step = int(input(f"Player {next(player)}, choose a number between 1 and 10 to add: "))
+			if not (1 <= step <= 10):
+				raise ValueError
+			break
+		except ValueError:
+			print("Invalid entry")
 	total += step
 	if total == 100:
 		print(f"Player {player} wins!")
