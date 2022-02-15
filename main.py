fe9c1d3e95fcca0f8da671031f8a854ee85fd54b
78352 <90336057+78352@users.noreply.github.com>
2022-02-15 02:01:13 +0000

    NBHS Pull and Merge

--- main.py
+++ main.py
@@ -6,19 +6,23 @@ class NumberError(Exception):
 total = 0
 player = itertools.cycle('12')
 
+# Game loop
 while True:
-	print(f"\nThe current total is {total}.")
-	step = int(input(f"Player {next(player)}, choose a number between 1 and 10 to add: "))
-	try:
-		if step < 0 or step > 10:
-			raise NumberError
-	except NumberError:
-		print(f"\nNumber not between 1 and 10")
-	if step > 0 and step <= 10:
-		total += step
+	# Validation loop
+	while True:
+		print(f"\nThe current total is {total}.")
+		try:
+			step = int(input(f"Player {next(player)}, choose a number between 1 and 10 to add: "))
+			if not (1 <= step <= 10):
+				next(player)
+				raise ValueError
+			break
+		except ValueError:
+			print("Invalid entry")
+	total += step
 	if total == 100:
 		print(f"Player {player} wins!")
 		break
-	
+
 	
 	
