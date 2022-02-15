c45d0d7139d1a7318a9bc5416e6e7edc5de9f25c
denricoNBHS <>
2022-02-15 19:59:41 +0000

    Fix player incrementing during validation step in main.py

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ player = itertools.cycle('12')
 
 # Game loop
 while True:
+	player_num = next(player)
 	# Validation loop
 	while True:
 		print(f"\nThe current total is {total}.")
 		try:
-			step = int(input(f"Player {next(player)}, choose a number between 1 and 10 to add: "))
+			step = int(input(f"Player {player_num}, choose a number between 1 and 10 to add: "))
 			if not (1 <= step <= 10):
 				raise ValueError
 			break
