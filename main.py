372b6a055d4dac8318d320e00cf52ee83f3f1c31
JeremiahFlores1 <>
2022-02-15 00:32:23 +0000

    fixed player turns

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ player = itertools.cycle('12')
 # Game loop
 while True:
 
-	# Validation loop
-	while True:
-		print(f"\nThe current total is {total}.")
-		try:
-			step = int(input(f"Player {next(player)}, choose a number between 1 and 10 to add: "))
-			if not (1 <= step <= 10):
-				raise ValueError
-			break
-		except ValueError:
-			print("Invalid entry")
-	total += step
-	if total == 100:
-		print(f"Player {player} wins!")
-		break
+  # Validation loop
+  while True:
+    print(f"\nThe current total is {total}.")
+    try:
+      step = int(input(f"Player {next(player)}, choose a number between 1 and 10 to add: "))
+      if not (1 <= step <= 10):
+        raise ValueError
+      break
+    except ValueError:
+      next(player)
+      print("Invalid entry")
+    else:
+      break
+  total += step
+  if total == 100:
+    print(f"Player {player} wins!")
+    break
